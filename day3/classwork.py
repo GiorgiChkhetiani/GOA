0026a17efff6cefa48a719e0e68495c944f05188
Giorgi Chkhetiani <123612013+GiorgiChkhetiani@users.noreply.github.com>
2023-09-29 23:27:54 +0400

    Add files via upload

---
+++ day3/classwork.py
@@ -0,0 +1,26 @@
+#here we learned debugging
+
+#name = "tom"
+#salary = 75000
+#print("name")
+
+
+#bug - ხარვეზი
+#debugging - ხარვეზის პოვნის პროცესი
+
+#message = "debugging"
+#print(message)
+
+#anticipated - მოსალოდნელი 
+#explanation - განმარტება
+#uppercase - A 
+#lowercase - a
+
+#nake case  is a popular wawy to creat in consistent name
+
+# input - რაც შედის კომპიუტერში 
+# output - რაც გამოდის 
+
+
+user_name = input(" what your name: " )
+print("hello " + user_name)
---
+++ day3/homework.py
@@ -0,0 +1,9 @@
+# name = input("what your name : " )
+# surename = input(" what your surname:" )
+# age = input(" what your age :" )
+# print (str ( name )  +  str ( surename)   +  str( age) )
+
+
+name = input("whats your name : ")
+new_name = int(name) + 25
+print(new_name)
