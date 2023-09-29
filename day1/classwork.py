0f07d6379ac8edc66f2fb219bab7f3091c02fde1
Giorgi Chkhetiani <123612013+GiorgiChkhetiani@users.noreply.github.com>
2023-09-29 19:13:44 +0400

    Add files via upload

---
+++ day1/classwork.py
@@ -0,0 +1,19 @@
+mother = 44
+father = 45
+
+mother += 5
+father += 5
+
+print( mother , father)
+
+
+
+age  = 22
+age1 = 27
+age2 = 43
+
+age  = age + 5
+age1 = age + 5
+age2 = age + 5 
+
+print(age ,age1 , age2)
