68d9ae7a0dff5357df6f3dea7e12a67c7e34ad6c
Giorgi Chkhetiani <123612013+GiorgiChkhetiani@users.noreply.github.com>
2023-10-04 02:09:06 +0400

    Add files via upload

---
+++ day4/classwork/homework.py
@@ -0,0 +1,46 @@
+
+# #1 დავალება
+# მომხმარებელს შემოატანინეთ ასაკი, შემოტანილი ასაკი გაამრავლეთ 7 ზე და გაყეთ  3 ზე და მიღებულ ასაკს დაუმატეთ  4 სა და ცხრის ნამრავლის ჯამი
+
+# #2 დავალება
+# მანქანის საწყისი ფასი იყო 7000ლ და გაძვირდა 62% ით, რამდენი ლარით მოიმატა მანქანის ღირებულებამ?  ამოხსენით პითონის საშუალებით
+
+
+
+
+
+# age = int(input("what is your age : " ))
+
+# math= (age * 7) / 3 + (4  *  9 )
+
+# print(math)a
+
+
+
+# car_price = 7000
+# new_car_price = (car_price / 100) * 62
+# print(new_car_price)
+
+
+
+# waitress = "what would you like to eat " 
+
+# print(waitress)
+
+# client = "khinkali"
+
+# print(client)
+# waitress = "how much "
+
+# print(waitress)
+
+# client = str(99)
+
+# print(client)
+
+# waitress = "maybe a hundred"
+
+# print ( waitress)
+
+# client = ("what a hundred ? am i pig ?" )
+# print(client)
