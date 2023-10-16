f93c02d69e77397d6fa0a0f71d7d51eac88a3d80
Giorgi Chkhetiani <123612013+GiorgiChkhetiani@users.noreply.github.com>
2023-10-17 02:41:44 +0400

    Add files via upload

---
+++ day7/classwork.py
@@ -0,0 +1,3 @@
+my_list = ["khinkali", "mwvadi" , "vashli", 7 , 2.5]
+print (my_list)
+
---
+++ day7/homework1.py
@@ -0,0 +1,4 @@
+# შექმენით თქვენი რაზმის წევრებისგან სია და დაპრინტეთ შუა 5 რაზმის წევრი(slicing)
+
+teammate_names = ["guri" , "giorgi", "salome" , "tekla " , "rati" "tsotne" "nino"]
+print(teammate_names[0:6])
