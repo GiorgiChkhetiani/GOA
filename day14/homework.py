0ea7dc85db2d932a6045785336e66c09dab8c829
Giorgi Chkhetiani <123612013+GiorgiChkhetiani@users.noreply.github.com>
2023-11-28 18:03:56 +0400

    Add files via upload

---
+++ day14/homework.py
@@ -0,0 +1,6 @@
+def summation(num):
+    sum = 0 
+    for i in range(num + 1):
+        if i  > 0 :
+            sum += i 
+    return sum
---
+++ day14/homework2.py
@@ -0,0 +1,7 @@
+def no_space(x):
+    new_str = ""
+    for i in x :
+        if i != " ":
+            new_str += i
+    return new_str
+            
